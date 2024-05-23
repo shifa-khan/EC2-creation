bf0ce8c0bd181a698c486716353fafd7842ba68f
Shifa Khan <67899867+shifa-khan@users.noreply.github.com>
2024-05-23 17:42:24 -0400

    main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test" {
+    ami = "ami-0fe630eb857a6ec83"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0ee57210a7468b509"
+    vpc_security_group_ids = ["sg-069101276e55a0360"]
+}
